ab54f9fbe37e9b58396339415135379ec5e702a9
Daniel Burchett <60920195+Dnlburch@users.noreply.github.com>
2025-04-09 22:28:50 -0500

    config database values
    explain/assume user creates database.ini file to loop through and provide connection details

---
+++ config.py
@@ -0,0 +1,18 @@
+from configparser import ConfigParser
+
+def config(filename="database.ini", section="postgresql"):
+    #create parser
+    parser = ConfigParser()
+    #read config
+    parser.read(filename)
+    #empty db dict to store key:value
+    db = {}
+    #check section matches
+    if parser.has_section(section):
+        params = parser.items(section)
+        #if matches loop through and build dict
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(f'Section:{section} is not found in the {filename} file.')
+    print(db)
