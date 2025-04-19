6fbbf205c8d6db623e30341d3162f1ab9740cfb7
Daniel Burchett <60920195+Dnlburch@users.noreply.github.com>
2025-04-19 14:12:27 -0500

    return db, add cursor close
    when you print but forget to return lol

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ def config(filename="database.ini", section="postgresql"):
             db[param[0]] = param[1]
     else:
         raise Exception(f'Section:{section} is not found in the {filename} file.')
-    print(db)
+    
+    return db
 
 config()
--- main.py
+++ main.py
@@ -5,17 +5,24 @@ def connect():
     connection = None
     try:
         params = config()
+        print(params)
         print('connting to postgreSQL...')
         connection = psycopg2.connect(**params)
 
-        #create a cursor
+        # create a cursor
         crsr = connection.cursor()
         print('PG database version: ')
         crsr.execute('SELECT version()')
         db_version = crsr.fetchone()
         print(db_version)
+        crsr.close()
+
     except(Exception, psycopg2.DataError) as error:
         print(error)
     finally:
         if connection != None:
             connection.close()
+            print('DB connection closed')
+
+if __name__ == '__main__':
+    connect()
